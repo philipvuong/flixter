77815086f9400116f188b584be01dbc6b4cd3d21
Philip Vuong <philipvuong@yahoo.com>
2018-08-25 17:23:11 +0000

    Challenge Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,8 @@
 class LessonsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  before_action :require_user_enrolled, only: [:show]
 
+  def show
   end
 
   private
@@ -9,4 +11,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_user_enrolled
+    if current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to lesson_path(current_lesson.section.course)
+    else
+      redirect_to course_path, alert: 'You must enroll before viewing this lesson'
+    end
+  end 
 end
